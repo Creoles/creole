55b605fbca238736e28d6e41402e967193c23cd5
EricQAQ <512629113@qq.com>
2017-02-17 10:28:44 +0800

    add enum type

--- creole/util.py
+++ creole/util.py
@@ -11,3 +11,41 @@ class CachedProperty(object):
 
 
 cached_property = CachedProperty
+
+
+class EnumItem(object):
+    def __init__(self, key, value, help=''):
+        self.key = key
+        self.value = value
+        self.help = help
+
+
+class Enum(object):
+    def __init__(self, **kwargs):
+        self._items = dict()
+        self._values = []
+        for item in kwargs:
+            if len(item) not in (2, 3):
+                raise ValueError(item)
+            elif item[1] in self._values:
+                raise ValueError('Duplicate value: %r', item[1])
+            self._items[item[0]] = EnumItem(**item)
+            self._values.append(item[1])
+
+    def __getattr__(self, key):
+        try:
+            return self._items[key].value
+        except KeyError:
+            raise AttributeError(key)
+
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+
+    def values(self):
+        return self._values
+
+    def keys(self):
+        return self._items.keys()
+
+    def items(self):
+        return self._items.iteritems()
