c8b26e90e89aff0f026e53e768f9d5ded9076d75
EricQAQ <512629113@qq.com>
2017-02-26 20:54:25 +0800

    add scripts

---
+++ scripts/db.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+import argparse
+from sqlalchemy import create_engine
+
+from creole.model import Base
+from creole.config import setting
+
+engine = create_engine(setting.DB_SETTINGS, echo=True)
+
+
+def create_all():
+    Base.metadata.create_all(engine)
+
+
+def drop_all():
+    Base.metadata.drop_all(engine)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--cmd', help='db command',
+                        choices=['create', 'drop', 'refresh', 'import'])
+    args = parser.parse_args()
+
+    if args.cmd and args.cmd == 'create':
+        create_all()
+    elif args.cmd and args.cmd == 'drop':
+        drop_all()
+    elif args.cmd and args.cmd == 'refresh':
+        drop_all()
+        create_all()
+
+if __name__ == '__main__':
+    main()
