cec297d6bbe57818f780985147e07d347a36a9a6
EricQAQ <512629113@qq.com>
2017-03-04 17:46:00 +0800

    Fix flask-restful

--- creole/wsgi/api/util.py
+++ creole/wsgi/api/util.py
@@ -1,7 +1,9 @@
+# coding: utf-8
 import logging
 import inspect
 
 from flask import request
+from flask_restful import Resource as BaseResource
 from flask_restful.reqparse import RequestParser, Argument
 from werkzeug.exceptions import BadRequest
 
@@ -9,19 +11,13 @@ from ...exc import raise_error_json, ParameterError
 
 logger = logging.getLogger(__name__)
 
+method_decorate_initialized = False
+
 
 class BaseRequestParser(RequestParser):
-
     def __init__(self, *args, **kwargs):
         super(BaseRequestParser, self).__init__(*args, **kwargs)
         self._add_customize_arguments()
-
-    def _collect_customize_arguments(self):
-        customize_arguments = []
-        for value in self.__class__.__dict__.values():
-            if isinstance(value, Argument):
-                customize_arguments.append(value)
-        return customize_arguments
 
     def _add_customize_arguments(self):
         for value in self.__class__.__dict__.values():
@@ -29,15 +25,13 @@ class BaseRequestParser(RequestParser):
                 self.add_argument(value)
 
 
-class ApiMixinMeta(type):
-    def __new__(cls, name, bases, attrs):
-        cls = type.__new__(cls, name, bases, attrs)
-        if name == 'ApiMixin':
-            return cls
-        _dict = attrs.get('meta', {})
-        args_parser_dict = _dict.get('args_parser', None)
+class Resource(BaseResource):
+    def _set_req_method_decorate(self):
+        self._req_method_decorate = {} 
+        _dict = getattr(self, 'meta', {})
+        args_parser_dict = _dict.get('args_parser_dict', None)
         if args_parser_dict is None:
-            return cls
+            return self._req_method_decorate
 
         def predicate(member):
             return (
@@ -64,17 +58,30 @@ class ApiMixinMeta(type):
 
                     # self
                     args[0].parsed_data = parsed_data
-                    return func(*args, **kwargs)
+                    return func(**kwargs)
 
                 return decorated
             return wrapper
 
-        for method_name, method in inspect.getmembers(cls, predicate):
+        for method_name, method in inspect.getmembers(self, predicate):
             parser_cls = args_parser_dict.get(method_name, None) or \
                 args_parser_dict.get('*', None)
             if parser_cls is not None:
-                cls.method_name = set_parser(parser_cls)(method)
+                self._req_method_decorate[method_name] = [set_parser(parser_cls)]
 
-
-class ApiMixin:
-    __meta__ = ApiMixinMeta
+    def _set_method_decorate(self):
+        global method_decorate_initialized 
+        if not method_decorate_initialized:
+            # flask-restulf==0.3.5目前还不支持针对不同的request method
+            # 加不同的decorate, 需要使用比较tricky的方法
+            # self.method_decorators = self._set_req_method_decorate()
+            for meth_name in ('post', 'put', 'get', 'delete'):
+                meth = getattr(self, meth_name, None)
+                if meth:
+                    self._set_req_method_decorate()
+                    setattr(self, meth_name, self._req_method_decorate[meth_name][0](meth))
+            method_decorate_initialized = True
+    
+    def dispatch_request(self, *args, **kwargs):
+        self._set_method_decorate()
+        super(Resource, self).dispatch_request(self, *args, **kwargs)
