f791da51a44be1b34c4237024fa7595433c68b01
EricQAQ <512629113@qq.com>
2017-04-14 11:59:26 +0800

    add gunicorn settings

--- creole/settings.py
+++ creole/settings.py
@@ -2,6 +2,8 @@ from .util import get_setting_manager
 
 setting_manager = get_setting_manager()
 
+CREOLE_HOST = setting_manager.get('CREOLE_HOST', '127.0.0.1')
+CREOLE_PORT = setting_manager.get_int('CREOLE_PORT', 8888)
 CREOLE_DB_URL = setting_manager.get('CREOLE_DB_URL',
         "mysql+pymysql://root:123456@localhost:3306/creole?charset=utf8")
 DB_SETTINGS = {
