85f0a02d063b132799439bf5678fe089d9030c5e
EricQAQ <512629113@qq.com>
2017-05-03 23:17:04 +0800

    update tourguide

--- creole/wsgi/api/v1/req_param/tour_guide.py
+++ creole/wsgi/api/v1/req_param/tour_guide.py
@@ -10,6 +10,7 @@ GUIDE_TYPE = Enum(
     ('DRIVER', 1, u'司机导游'),
     ('ATTRACTION', 2, u'景点导游'),
     ('TRANSLATOR', 3, u'翻译'),
+    ('LEADER', 4, u'领队'),
 )
 
 GENDER = Enum(
@@ -30,22 +31,48 @@ class CreateTourGuideApiParser(BaseRequestParser):
         ('ID', 1, u'身份证'),
         ('PASSPORT', 2, u'护照'),
     )
+    LANGUAGE_LEVEL = Enum(
+        ('EXCELLENT', 1, u'熟练'),
+        ('VERY_GOOD', 2, u'优秀'),
+        ('GOOD', 3, u'良好'),
+        ('FAIR', 4, u'及格'),
+        ('POOR', 5, u'差'),
+    )
+    PASSPORT_TYPE = Enum(
+        ('TRAVEL', 1, u'旅游签证'),
+        ('BUSINESS', 2, u'商务签证'),
+        ('WORK', 3, u'工作签证'),
+    )
 
     guide_type = Argument(
         'guide_type', type=int, choices=GUIDE_TYPE.values(), required=True)
     country_id = Argument('country_id', type=int, nullable=False, required=True)
     name = Argument('name', required=False)
-    name_en = Argument('name_en', type=str, required=False)
+    name_en = Argument('name_en', type=str, nullable=False, required=True)
+    nickname_en = Argument('nickname_en', type=str, nullable=False, required=True)
     gender = Argument('gender', type=int, choices=GENDER.values(), required=True)
     birthday = Argument('birthday', nullable=False, required=True, type=int)
     start_work = Argument('start_work', type=int, nullable=False, required=True)
-    language = Argument('language', nullable=False, required=True)
+    first_language = Argument('first_language', nullable=False, required=True)
+    first_language_level = Argument(
+        'first_language_level', choices=LANGUAGE_LEVEL.values(), nullable=False, required=True)
+    second_language = Argument('second_language', nullable=False, required=True)
+    second_language_level = Argument(
+        'second_language_level', choices=LANGUAGE_LEVEL.values(), nullable=False, required=True)
+    third_language = Argument('third_language', required=False)
+    third_language_level = Argument(
+        'third_language_level', choices=LANGUAGE_LEVEL.values(), required=False)
     certificate_type = Argument(
         'certificate_type', type=int, choices=CERTIFICATE_TYPE.values(), required=True)
     certificate_number = Argument('certificate_number', nullable=False, required=True)
     tour_guide_number = Argument('tour_guide_number', nullable=False, required=True)
-    passport_country = Argument('passport_country')
-    telephone=Argument('telephone', nullable=False, required=True)
+    passport_country = Argument('passport_country', nullable=False, required=True)
+    passport_type = Argument('passport_type', choices=PASSPORT_TYPE.values(), required=True)
+    passport_note = Argument('passport_note', required=False)
+    telephone_one=Argument('telephone_one', nullable=False, required=True)
+    telephone_two=Argument('telephone_two', required=False)
+    email = Argument('email')
+    company_id = Argument('company_id', type=int, nullable=False, required=True)
     intro = Argument('intro')
     image_hash = Argument('image_hash', required=True)
 
